c0a515fc23b7bb3b78746de4cee0122a25485d88
Bradcj <j.chaiyaphop@gmail.com>
2022-03-20 22:06:15 +0700

    Revise the script

--- Churn_Prediction/churn_pred.py
+++ Churn_Prediction/churn_pred.py
@@ -1,4 +1,6 @@
+from msilib.schema import Feature
 from matplotlib.pyplot import fill
+from matplotlib.transforms import Transform
 import pandas as pd
 from scipy.fftpack import sc_diff
 import seaborn as sns
@@ -13,17 +15,20 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import StandardScaler
 
 
+# Import Churn Data i
 df = pd.read_csv(r'/mnt/c/Users/hpt-de/Documents/Central_Project/ML_Models/Churn_Prediction/CHURN_DATA.csv', dtype='unicode')
-# print(df[['CHERN_FLAG']])
-
-X = df[['TD_VISIT','CFH_VISIT','TM_VISIT','TOTAL']]
-Y = df[['CHERN_FLAG']]
 
 
+# Separate Feature and Target
+X = df[["OLYMPIC_SEGMENT","TOTAL","GP","BUSKET_SIZE","TOTAL_PRODUCT","LAST_VISIT_DATE","LAPSED_DAYS","CFH_VISIT","CFH_TOTAL_VISIT","CFH_LAST_VISIT_DATE","TD_VISIT","TD_TOTAL_VISIT","TD_LAST_VISIT_DATE","TM_VISIT","TM_TOTAL_VISIT","TM_LAST_VISIT_DATE"]]
+Y = df[["CHERN_FLAG"]]
+
+
+# Encode Churn Data to Binary Format
 label_encoder = LabelEncoder()
 
 # Encoding X
-encoding_x = ['TD_VISIT','CFH_VISIT','TM_VISIT','TOTAL']
+encoding_x = ["OLYMPIC_SEGMENT","TOTAL","GP","BUSKET_SIZE","TOTAL_PRODUCT","LAST_VISIT_DATE","LAPSED_DAYS","CFH_VISIT","CFH_TOTAL_VISIT","CFH_LAST_VISIT_DATE","TD_VISIT","TD_TOTAL_VISIT","TD_LAST_VISIT_DATE","TM_VISIT","TM_TOTAL_VISIT","TM_LAST_VISIT_DATE"]
 x_encoded = pd.DataFrame()
 for i in encoding_x:
     x_encoded[i] = pd.to_numeric(label_encoder.fit_transform(X[i]))
@@ -31,18 +36,27 @@ print(x_encoded.head())
 
 #  Encoding Y
 y_encoded = pd.DataFrame()
-y_encoded['CHERN_FLAG'] = pd.to_numeric(label_encoder.fit_transform(Y['CHERN_FLAG']))
+y_encoded["CHERN_FLAG"] = pd.to_numeric(label_encoder.fit_transform(Y['CHERN_FLAG']))
 print(y_encoded.head())
 
-# scaler = StandardScaler()
-# scaler.fit(x_encoded)
-# x_sca = scaler.transform(x_encoded)
-# x_sca = pd.DataFrame(x_sca, columns=['TD_VISIT','CFH_VISIT','TM_VISIT','TOTAL'])
 
-# sns.pairplot(x_sca.iloc[:, 0:5])
+# Create New DataFrame
+df = pd.concat([x_encoded,y_encoded], axis=1)
 
 
-X_train, X_test, y_train, y_test = train_test_split(x_encoded, y_encoded, train_size = .8, random_state = 101)
+# Pair Plot Graph
+sns.pairplot(df, vars=["OLYMPIC_SEGMENT","TOTAL","GP","BUSKET_SIZE","TOTAL_PRODUCT","LAST_VISIT_DATE","LAPSED_DAYS","CFH_VISIT","CFH_TOTAL_VISIT","CFH_LAST_VISIT_DATE","TD_VISIT","TD_TOTAL_VISIT","TD_LAST_VISIT_DATE","TM_VISIT","TM_TOTAL_VISIT","TM_LAST_VISIT_DATE","CHERN_FLAG"], hue="CHERN_FLAG")
+
+
+# Transform Data with Scaler 
+scaler = StandardScaler()
+scaler.fit(x_encoded)
+x_sca = scaler.transform(x_encoded)
+x_sca = pd.DataFrame(x_sca, columns=["OLYMPIC_SEGMENT","TOTAL","GP","BUSKET_SIZE","TOTAL_PRODUCT","LAST_VISIT_DATE","LAPSED_DAYS","CFH_VISIT","CFH_TOTAL_VISIT","CFH_LAST_VISIT_DATE","TD_VISIT","TD_TOTAL_VISIT","TD_LAST_VISIT_DATE","TM_VISIT","TM_TOTAL_VISIT","TM_LAST_VISIT_DATE"])
+
+
+# Train and Prediction Model
+X_train, X_test, y_train, y_test = train_test_split(x_sca, y_encoded, train_size = .8, random_state = 101)
 
 algo = [
     [LogisticRegression(solver='lbfgs'), 'LogisticRegression'],
@@ -57,29 +71,30 @@ for a in algo:
     y_pred=model.predict(X_test) 
     score=model.score(X_test, y_test)
     model_score.append([score, a[1]])
-    print(f'{a[1]} score = {score}') 
+    print(f'{a[1]} score = {score}')
     print(metrics.confusion_matrix(y_test, y_pred))
     print(metrics.classification_report(y_test, y_pred))
     print('-' * 100)
-# print(model_score)
 
 print(pd.DataFrame(model_score, columns=['score', 'model']).sort_values(by='score', ascending=False))
 
 
-# clf = tree.DecisionTreeClassifier()
-# model = clf.fit(x_encoded, y_encoded)
+# Plot Tree Graph
+clf = tree.DecisionTreeClassifier()
+model = clf.fit(x_sca, y_encoded)
 
-# print(model)
+fn = ["OLYMPIC_SEGMENT","TOTAL","GP","BUSKET_SIZE","TOTAL_PRODUCT","LAST_VISIT_DATE","LAPSED_DAYS","CFH_VISIT","CFH_TOTAL_VISIT","CFH_LAST_VISIT_DATE","TD_VISIT","TD_TOTAL_VISIT","TD_LAST_VISIT_DATE","TM_VISIT","TM_TOTAL_VISIT","TM_LAST_VISIT_DATE"]
+
+tree.plot_tree(model,
+                feature_names = fn,
+                filled=True)
+
+tree.export_graphviz(model,
+                    out_file=r'/mnt/c/Users/hpt-de/Documents/Central_Project/ML_Models/Churn_Prediction/D_Tree.dot',
+                    feature_names = fn,
+                    filled = True)
 
 
-# fn = ['TD_VISIT','CFH_VISIT','TM_VISIT','TOTAL']
-
-# tree.plot_tree(model,
-#                 feature_names = fn,
-#                 filled=True)
-
-# tree.export_graphviz(model,
-#                     out_file=r'/mnt/c/Users/hpt-de/Documents/Central_Project/ML_Models/Churn_Prediction/tree.dot',
-#                     feature_names = fn,
-#                     filled = True)
-                
+# Status Detail
+model_best = sm.Logit(y_encoded,x_sca).fit()
+model_best.summary2()
